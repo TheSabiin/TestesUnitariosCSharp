287d1f567f98da719693eb8d6da832493ad7230e
ivinib <vi_ni_b@hotmail.com>
2018-11-08 10:53:41 -0200

    Add files via upload

---
+++ TextFixturecs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoMvc.UnitTests
+{
+    class TextFixturecs
+    {
+    }
+}
---
+++ ToDoItemServiceShould.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using TodoMvc.Data;
+using TodoMvc.Models;
+using ToDoMvc.Services;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace ToDoMvc.UnitTests
+{
+    class ToDoItemServiceShould
+    {
+        [Fact]
+        public async Task AddNewItemAsIncompleteWithDueDate()
+        {
+           var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "Test_AddNewItem")
+            .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new ToDoItemService(context);
+
+                var fakeUser = new ApplicationUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new NewToDoItem
+                {
+                    Title = "Testing?",
+                    DueAt = DateTimeOffset.Now.AddDays(3)
+                }, fakeUser);
+            }
+        }
+        
+    }
+}
